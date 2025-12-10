4baf08daaae7aa18fb7a446e6e4c71673b5b202b
xianliangwong <xianliangwong0620@gmail.com>
2025-12-10 17:04:10 +0800

    change the CORS origin policy for frontend to get the cookies refresh token

--- src/main/java/com/asinglah/backend/HelperClass/SecurityConfig.java
+++ src/main/java/com/asinglah/backend/HelperClass/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -58,5 +61,20 @@ public class SecurityConfig {
     ; // instead of formLogin
 
         return http.build();
+    }
+
+    @Bean 
+    public CorsConfigurationSource corsConfigurationSource() 
+    { 
+        CorsConfiguration config = new CorsConfiguration(); 
+         
+        config.setAllowCredentials(true); 
+        //add the allow origin
+        config.addAllowedOrigin("http://localhost:4200"); 
+        config.addAllowedHeader("*"); 
+        config.addAllowedMethod("*"); 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); 
+        return source; 
     }
 }
