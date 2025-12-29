8d19a175f2ed3dbbd612c76e16c0b9d9bb51fa6c
xianliangwong <xianliangwong0620@gmail.com>
2025-12-29 16:11:54 +0800

    created the native query implementation to get group expense from userid

--- src/main/java/com/asinglah/backend/Repository/ExpenseGroupRepository.java
+++ src/main/java/com/asinglah/backend/Repository/ExpenseGroupRepository.java
@@ -1,9 +1,21 @@
 package com.asinglah.backend.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.asinglah.backend.DTO.ExpesenResponseDTO.ListExpenseGroupDTO;
 import com.asinglah.backend.Entity.Expense_group;
+import java.util.List;
+
 
 public interface ExpenseGroupRepository extends JpaRepository<Expense_group,Long>{
+
+    // @Query(value="SELECT",nativeQuery=true)
+    // List<ListExpenseGroupDTO> findMemberGroupDTOs(@Param("userId") long userId);
+
+    @Query(value="SELECT eg.group_name AS groupName,eg.expense_group_id AS expenseGroupId FROM expense_group eg LEFT JOIN group_member gm ON eg.expense_group_id = gm.expense_group_id WHERE (gm.user_id = :userId AND gm.statusid = 2) OR eg.group_owner_id = :userId"
+    , nativeQuery = true)
+    List<ListExpenseGroupDTO> findMemberGroupDTOs(@Param("userId") long userId);
 
 }
