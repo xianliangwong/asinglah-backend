da62168e0dd05aeb902454503247615d8e7d11ed
xianliangwong <xianliangwong0620@gmail.com>
2025-12-29 16:10:33 +0800

    commented wrong implementation of jpql

--- src/main/java/com/asinglah/backend/Repository/GroupMemberRepository.java
+++ src/main/java/com/asinglah/backend/Repository/GroupMemberRepository.java
@@ -1,21 +1,18 @@
 package com.asinglah.backend.Repository;
 
-import java.util.List;
+
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.asinglah.backend.DTO.ExpesenResponseDTO.ListExpenseGroupDTO;
 import com.asinglah.backend.Entity.group_member;
 
 public interface GroupMemberRepository extends JpaRepository<group_member,Long>{
 
     //jpql implementation 
-    @Query("SELECT new com.asinglah.backend.DTO.ExpesenResponseDTO.ListExpenseGroupDTO(eg.groupName, eg.expenseGroupId) " 
-    + "FROM group_member gm JOIN gm.expense_group eg WHERE gm.user_id.id = :userId AND gm.statusID.statusID =2") 
-    List<ListExpenseGroupDTO> findMemberGroupDTOs(@Param("userId") long userId);
+    // @Query("SELECT new com.asinglah.backend.DTO.ExpesenResponseDTO.ListExpenseGroupDTO(eg.groupName, eg.expenseGroupId) " 
+    // + "FROM group_member gm RIGHT JOIN gm.expense_group eg WHERE (gm.user_id.id = :userId AND gm.statusID.statusID =2) OR (eg.groupOwnerId.id=:userId)") 
+    // List<ListExpenseGroupDTO> findMemberGroupDTOs(@Param("userId") long userId);
 
 
 
